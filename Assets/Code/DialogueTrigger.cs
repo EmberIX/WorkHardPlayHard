e1471531046af1742c955f8822a7ce8f04d018e5
Sakjin <sakjin_s@sci.tu.ac.th>
2021-02-13 00:09:07 +0700

    DialogueCode

---
+++ Assets/Code/DialogueTrigger.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DialogueTrigger : MonoBehaviour
+{
+    public Dialogue dialogue;
+    public DialogueManager DM;
+    public GameObject E;
+    public bool isAtPosition;
+    public bool isDialogue;
+    PlayerScript PS;
+
+    void Start()
+    {
+        PS = GameObject.FindObjectOfType<PlayerScript>();
+    }
+
+    public void Update()
+    {
+
+        if (Input.GetKeyDown("e") && isAtPosition && isDialogue && DM.sentences.Count != 0)
+        {
+            DM.DisplayNextSentence();
+            PS.isInteracted = true;
+        }
+        else if (Input.GetKeyDown("e") && isDialogue && DM.sentences.Count == 0)
+        {
+            DM.EndDialogue();
+            PS.isInteracted = false;
+            return;
+        }
+
+        if (Input.GetKeyDown("e") && isAtPosition && isDialogue == false)
+        {
+            TriggerDialogue();
+            PS.isInteracted = true;
+
+        }
+       
+    }
+
+    public void TriggerDialogue()
+    {
+        DM.StartDialogue(dialogue);
+        isDialogue = true;
+    }
+
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag == ("Player"))
+        {
+            isAtPosition = true;
+            E.SetActive(true);
+
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.tag == ("Player"))
+        {
+
+            isAtPosition = false;
+            E.SetActive(false);
+
+        }
+    }
+}
