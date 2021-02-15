bc3dafeb0124ca88ff0afb8ce5258c39063409b8
Sakjin <sakjin_s@sci.tu.ac.th>
2021-02-15 22:20:04 +0700

    NoteBookPick

---
+++ Assets/Code/NoteBookPick.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NoteBookPick : MonoBehaviour
+{
+
+    public GameObject noteBook;
+    public PlayerScript PS;
+
+    void Start()
+    {
+        PS = GameObject.FindObjectOfType<PlayerScript>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            Instantiate(noteBook, transform.position, Quaternion.identity);
+            PS.weapon = "noteBook";
+            Destroy(this.gameObject);
+        }
+    }
+}
