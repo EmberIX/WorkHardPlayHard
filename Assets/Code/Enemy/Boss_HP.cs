873934360a2fad11d55a0935c70ecbb97a7b45f7
Sakjin <sakjin_s@sci.tu.ac.th>
2021-02-25 05:13:31 +0700

    Bot State Machine Ongoing

---
+++ Assets/Code/Enemy/Boss_HP.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Boss_HP : MonoBehaviour
+{
+    public float HP;
+    public float MaxHP;
+
+    public GameObject HPB;
+    public Image HPBar;
+    public bool TakeHit;
+    
+    public bool isIFlame;
+    public PlayerScript Ps;
+    private void Start()
+    {
+        Ps = GameObject.FindObjectOfType<PlayerScript>();
+    }
+
+    void TakeDamage(float amount)
+    {
+        if(isIFlame)
+        {
+            return;
+        }
+
+        HP -= amount;
+
+        if(HP <= 0)
+        {
+            Die();
+        }
+    }
+
+    public void SetHPBar()
+    {
+        HPBar.fillAmount = HP / MaxHP;
+    }
+
+    void Die()
+    {
+        Destroy(GameObject.FindGameObjectWithTag("BigSlime"));
+        Destroy(GameObject.FindGameObjectWithTag("smallBug"));
+        Destroy(GameObject.FindObjectOfType<Trap>());
+        Destroy(GameObject.FindObjectOfType<EnemyBullet>());
+        Destroy(HPB);
+
+        Destroy(this.gameObject);
+
+        SoundManagerScript.PlaySound(SoundManagerScript.enemyDie);
+        if (Ps != null)
+        {
+            Ps.HP = Ps.MaxHP;
+        }
+    }
+}
---
+++ Assets/Code/Enemy/Bot_Boss.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bot_Boss : MonoBehaviour
+{
+    public Transform player;
+    public PlayerScript Ps;
+
+    Vector2 target;
+    void Start()
+    {
+        Ps = GameObject.FindObjectOfType<PlayerScript>();
+        player = GameObject.FindGameObjectWithTag("Player").transform;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Ps.isDead != true)
+        {
+            if (player != null)
+            {
+                target = new Vector2(player.position.x, player.position.y);
+            }
+        }
+    }
+}
