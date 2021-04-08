7fc1c8844a909001aa3af689757fc0a0eb7e579b
Sakjin <sakjin_s@sci.tu.ac.th>
2021-04-08 19:46:57 +0700

    Error Bullet Spawn Random In Area

--- Assets/Code/Enemy/TrapBullet.cs
+++ Assets/Code/Enemy/TrapBullet.cs
@@ -37,15 +37,6 @@ public class TrapBullet : MonoBehaviour
     {
         bulletTime += Time.deltaTime;
 
-        //if (PM != null)
-        //{
-        //    transform.position = Vector2.MoveTowards(transform.position, target, speed * Time.deltaTime);
-        //}
-
-        //if (transform.position.x == target.x && transform.position.y == target.y)
-        //{
-        //    DestroyProjectile();
-        //}
 
         if (bulletTime >= bulletOut)
         {
---
+++ Assets/Code/Work/PlayerMini/ErrorBulletSpawn_Work.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ErrorBulletSpawn_Work : MonoBehaviour
+{
+    public Vector3 whereToSpawn;
+    public  GameObject bullet;
+
+    public float randomX;
+    public float spawnRate = 2f;
+    public float nextSpawn;
+
+    void Start()
+    {
+        
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       
+        if(Time.time > nextSpawn)
+        {
+            nextSpawn = Time.time + spawnRate;
+            randomX = Random.Range(1f, 5f);
+            whereToSpawn = new Vector3(randomX, transform.position.y, transform.position.z);
+            
+            GameObject SpawnedBullet = Instantiate(bullet, whereToSpawn, Quaternion.identity);
+            //SpawnedBullet.GetComponent<Rigidbody2D>().velocity = this.transform.up * 12f;
+            SpawnedBullet.transform.SetParent(GameObject.FindGameObjectWithTag("Canvas_Work").transform);
+        }
+
+    }
+}
